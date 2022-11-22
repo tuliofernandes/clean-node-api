e5f179be200ad7fd662a1a53c39f28f7d6fdc18d
Túlio Fernandes <tuliomf09@gmail.com>
2022-11-22 14:09:57 -0300

    chore: lock mongodb version to 4.0.1

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: 'latest',
+      version: '4.0.1',
       skipMD5: true
     },
     autoStart: false,
     instance: {}
-  }
+  },
+  mongoURLEnvName: 'MONGO_TEST_URI'
 }
